8492cf88c448e64e265cd49fd437c2a892a1eb38
colin <colint.cron@gmail.com>
2025-11-20 13:02:18 -0500

    Initial Commit

---
+++ FastCrawl/settings.py
@@ -0,0 +1,88 @@
+# Scrapy settings for FastCrawl project
+#
+# For simplicity, this file contains only settings considered important or
+# commonly used. You can find more settings consulting the documentation:
+#
+#     https://docs.scrapy.org/en/latest/topics/settings.html
+#     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
+#     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+
+BOT_NAME = "FastCrawl"
+
+SPIDER_MODULES = ["FastCrawl.spiders"]
+NEWSPIDER_MODULE = "FastCrawl.spiders"
+
+ADDONS = {}
+
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = "FastCrawl (+http://www.yourdomain.com)"
+
+# Obey robots.txt rules
+ROBOTSTXT_OBEY = False
+
+# Concurrency and throttling settings
+#CONCURRENT_REQUESTS = 16
+CONCURRENT_REQUESTS_PER_DOMAIN = 1
+DOWNLOAD_DELAY = 1
+
+# Disable cookies (enabled by default)
+#COOKIES_ENABLED = False
+
+# Disable Telnet Console (enabled by default)
+#TELNETCONSOLE_ENABLED = False
+
+# Override the default request headers:
+DEFAULT_REQUEST_HEADERS = {
+    "Accept": "text/html",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/109.0",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Referer": "127.0.0.1"
+}
+# Enable or disable spider middlewares
+# See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+#SPIDER_MIDDLEWARES = {
+#    "FastCrawl.middlewares.FastcrawlSpiderMiddleware": 543,
+#}
+
+# Enable or disable downloader middlewares
+# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
+#DOWNLOADER_MIDDLEWARES = {
+#    "FastCrawl.middlewares.FastcrawlDownloaderMiddleware": 543,
+#}
+
+# Enable or disable extensions
+# See https://docs.scrapy.org/en/latest/topics/extensions.html
+#EXTENSIONS = {
+#    "scrapy.extensions.telnet.TelnetConsole": None,
+#}
+
+# Configure item pipelines
+# See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+#ITEM_PIPELINES = {
+#    "FastCrawl.pipelines.FastcrawlPipeline": 300,
+#}
+
+# Enable and configure the AutoThrottle extension (disabled by default)
+# See https://docs.scrapy.org/en/latest/topics/autothrottle.html
+#AUTOTHROTTLE_ENABLED = True
+# The initial download delay
+#AUTOTHROTTLE_START_DELAY = 5
+# The maximum download delay to be set in case of high latencies
+#AUTOTHROTTLE_MAX_DELAY = 60
+# The average number of requests Scrapy should be sending in parallel to
+# each remote server
+#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# Enable showing throttling stats for every response received:
+#AUTOTHROTTLE_DEBUG = False
+
+# Enable and configure HTTP caching (disabled by default)
+# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
+#HTTPCACHE_ENABLED = True
+#HTTPCACHE_EXPIRATION_SECS = 0
+#HTTPCACHE_DIR = "httpcache"
+#HTTPCACHE_IGNORE_HTTP_CODES = []
+#HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
+
+# Set settings whose default value is deprecated to a future-proof value
+FEED_EXPORT_ENCODING = "utf-8"
---
+++ FastCrawl/spiders/fastcrawl_spider.py
@@ -0,0 +1,136 @@
+import scrapy, sqlite3, socket, requests, re
+from bs4 import BeautifulSoup
+
+def write_to_domain_database(name):
+    conn = sqlite3.connect("ScrapyDataBase", isolation_level=None)
+    conn.execute('CREATE TABLE IF NOT EXISTS Domains ("url" TEXT NOT NULL)')
+    print(" Checking for " + name + " in database")
+    entry_exists = conn.execute("SELECT DISTINCT url FROM Domains WHERE url='{}'".format(name))
+    db_result = str(entry_exists.fetchall()).replace("[('","").replace("',)]","")
+    # print("DB_RESULT:::   " + db_result)
+    if db_result == name:
+        print("\n" + name + " is already in DB")
+        return
+    else:
+        sql = """INSERT INTO Domains (url)
+                                VALUES ('{}');""".format(name)
+        conn.execute(sql)
+        print("\n" + name + " saved to database")
+        return
+
+def write_to_info_database(name, ip, content_type, x_frame_opts,
+                           x_xss_prot, server, x_cont_type,
+                           referrer_policy, access_control_allow_origin):
+    print("Attempting to write to Info Database")
+    conn = sqlite3.connect("ScrapyDataBase", isolation_level=None)
+
+    entry_exists = conn.execute("SELECT DISTINCT url FROM Info WHERE url='{}'".format(name))
+    db_result = str(entry_exists.fetchall()).replace("[('", "").replace("',)]", "")
+    if db_result == name:
+        print("\n" + name + " is already in DB")
+        return
+    else:
+        conn.execute('''CREATE TABLE IF NOT EXISTS Info ("url" TEXT NOT NULL,
+                                                        "ip_address" TEXT NOT NULL,
+                                                        "content_type" TEXT,
+                                                        "x_frame_options" TEXT,
+                                                        "x_xss_protection" TEXT,
+                                                        "server" TEXT,
+                                                        "x_content_type_options" TEXT,
+                                                        "referrer_policy" TEXT,
+                                                        "access_control_allow_origin" TEXT)''')
+        # print("DB_RESULT:::   " + db_result)
+        sql = """INSERT INTO Info (url, ip_address, content_type, x_frame_options, 
+                               x_xss_protection, server, x_content_type_options,
+                               referrer_policy, access_control_allow_origin)
+                                VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}');""".format(name, ip, content_type, x_frame_opts,
+                                                                     x_xss_prot, server, x_cont_type, referrer_policy,
+                                                                     access_control_allow_origin)
+        conn.execute(sql)
+        print("\n" + name + " saved to database")
+        return
+
+
+def sanitize_url(url):
+    sanitized = ""
+    if url.startswith("https://") and url.endswith("/"):
+        sanitized = url.replace("https://", "")
+        sanitized = sanitized[:-1]
+    elif url.startswith("http://") and url.endswith("/"):
+        sanitized = url.replace("https://", "")
+        sanitized = sanitized[:-1]
+    elif url.startswith("https://"):
+        sanitized = url.replace("https://", "")
+    elif url.startswith("http://"):
+        sanitized = url.replace("http://", "")
+    return sanitized
+
+
+def get_server_info(domain_name):
+    print("\nGetting server info")
+    header_response = requests.head(domain_name)
+    ip = ""
+    content_type = ""
+    x_frame_options = ""
+    x_xss_protection = ""
+    server = ""
+    x_content_type_options = ""
+    referrer_policy = ""
+    access_control_allow_origin = ""
+
+    try:
+        server = header_response.headers['server']
+        content_type = header_response.headers['Content-Type']
+        x_frame_options = header_response.headers['X-Frame-Options']
+        x_xss_protection = header_response.headers['X-XSS-Protection']
+        x_content_type_options = header_response.headers['X-Content-Type-Options']
+        referrer_policy = header_response.headers['Referrer=Policy']
+        access_control_allow_origin = header_response.headers['Access-Control-Allow-Origin']
+
+    except KeyError as err:
+        print(err)
+
+    ip = socket.gethostbyname(sanitize_url(domain_name))
+
+    # write_to_domain_database(str(domain_name), ip, server, content_type, title)
+    if len(ip) > 0:
+        print("IP Address::  " + ip)
+        print("Server::  " + server)
+        print("Content_Type::  " + content_type)
+        print("X-Frame-Options::  " + x_frame_options)
+        print("X-XSS-Protection::  " + x_xss_protection)
+        print("X-Content-Type-Options::  " + x_content_type_options)
+        print("Referrer-Policy::  " + referrer_policy)
+        print("Access-Control-Allow-Origin::  " + access_control_allow_origin)
+        write_to_info_database(domain_name, ip, content_type, x_frame_options, x_xss_protection,
+                               server, x_content_type_options, referrer_policy,
+                               access_control_allow_origin)
+    else:
+        print("NO IP")
+
+
+class FastCrawlSpider(scrapy.Spider):
+    name = "FastCrawl" # identifies spider, must be unique within project
+
+    async def start(self):
+        urls = ["https://www.ebay.com"]
+        for url in urls:
+            print("URL IN URLS LOOP")
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        links = response.css("a::attr(href)").getall()
+        tld_list = (".com/", ".gov/", ".net/", ".edu/", ".org/",
+                    ".io/", ".co.uk/", ".ie/", ".info/", ".me/",
+                    ".in", ".info/", ".fr/", ".de/")
+        for link in links:
+            if link.startswith("https://www.linkedin.com/jobs/"):
+                return
+            else:
+                next_page = response.urljoin(link)
+                write_to_domain_database(next_page)
+                # email_scraper(response)
+                if next_page.endswith(tld_list):
+                    get_server_info(next_page)
+                yield scrapy.Request(next_page, callback=self.parse)
+
